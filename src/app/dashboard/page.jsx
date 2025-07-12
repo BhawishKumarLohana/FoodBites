2573645ca1d18f6776351c8c28f27f69a80550a2
Bhawish Kumar <23205172@life.hkbu.edu.hk>
2025-07-12 15:58:54 +0500

    database_init

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='bg-white w-full h-full'>
+        <div>Dashboard Page</div>
+
+
+    </div>
+  )
+}
+
+export default Dashboard
